b6796dd5ebbdde78a3eb2050debf704cbbaf1ae8
Yuechen Li <yuechenli@outlook.com>
2026-01-18 01:09:55 -0800

    Working sample.

---
+++ sample/ThirdPersonPlatformerInputManDemo/ThirdPersonPlatformerInputManDemo.Game/InstallInputMan.cs
@@ -0,0 +1,20 @@
+﻿using InputMan.Core;
+using InputMan.StrideConn;
+using Stride.Engine;
+
+namespace ThirdPersonPlatformerInputManDemo;
+
+public sealed class InstallInputMan : StartupScript
+{
+    public override void Start()
+    {
+        if (Game.Services.GetService<IInputMan>() != null)
+            return;
+
+        var profile = DefaultPlatformerProfile.Create();
+
+        // Install InputMan system directly (works with IGame)
+        var sys = new StrideInputManSystem(Game.Services, profile);
+        Game.GameSystems.Add(sys);
+    }
+}
