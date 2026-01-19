c72e25d08139c4b6af6bbd64b7b9010bd332f0de
Yuechen Li <yuechenli@outlook.com>
2026-01-19 05:12:27 -0800

    M2: Load profile via JSON Done.

--- sample/ThirdPersonPlatformerInputManDemo/ThirdPersonPlatformerInputManDemo.Game/InstallInputMan.cs
+++ sample/ThirdPersonPlatformerInputManDemo/ThirdPersonPlatformerInputManDemo.Game/InstallInputMan.cs
@@ -1,7 +1,9 @@
 ﻿using InputMan.Core;
+using InputMan.Core.Serialization;
 using InputMan.StrideConn;
 using Stride.Core.Mathematics;
 using Stride.Engine;
+using System.IO;
 
 namespace ThirdPersonPlatformerInputManDemo;
 
@@ -10,8 +12,16 @@ public sealed class InstallInputMan : StartupScript
 
     public override void Start()
     {
-        var sys = new StrideInputManSystem(Game.Services, DefaultPlatformerProfile.Create());
-        //sys.Enabled = true;
+        if (Game.Services.GetService<IInputMan>() != null)
+            return;
+
+        var json = File.ReadAllText("Resources/Input/profile.json");
+        var profile = InputProfileJson.Load(json);
+
+        var sys = new StrideInputManSystem(Game.Services, profile)
+        {
+            Enabled = true
+        };
         Game.GameSystems.Add(sys);
     }
 
